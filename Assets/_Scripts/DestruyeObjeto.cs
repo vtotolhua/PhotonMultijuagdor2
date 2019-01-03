2856e59680c7c74be2fa8a37e34d28c4b6a1eed9
Vladimir Totolhua <vtotolhua@gmail.com>
2019-01-03 16:45:15 -0600

    Salud

---
+++ Assets/_Scripts/DestruyeObjeto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestruyeObjeto : MonoBehaviour {
+
+    public float tiempoDestruccion = 2f;
+	// Use this for initialization
+	void Start () {
+        Invoke("Destruye", tiempoDestruccion);
+	}
+
+    //Funcion de destruir
+    void Destruye()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/_Scripts/Salud.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salud : MonoBehaviour {
+    public GameObject ParticulasMuerte = null;
+    private Transform ThisTransform = null;
+    public bool QuieresDestruiralMorir = false;
+
+	// Use this for initialization
+	void Start () {
+        ThisTransform = GetComponent<Transform>();
+	}
+
+    //
+	public float PuntosSalud
+    {
+        get
+        {
+            return _PuntosSalud;
+        }
+        set
+        {
+            _PuntosSalud = value;
+
+            if (_PuntosSalud <= 0.0f)
+            {
+                SendMessage("Morir", SendMessageOptions.DontRequireReceiver);
+
+                if (ParticulasMuerte != null) Instantiate(ParticulasMuerte, ThisTransform.position, ThisTransform.rotation);
+
+                if (QuieresDestruiralMorir) Destroy(gameObject);
+            }
+        }      
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) PuntosSalud = 0;
+    }
+
+    [SerializeField]
+    private float _PuntosSalud = 100f;
+}
